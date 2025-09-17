19d661509520c765a54cedb21408f047efdd30f1
andrea <andrea.isabelgomero@gmail.com>
2025-09-17 16:43:58 -0600

    otras pruebas

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,5 +3,9 @@ import { test, expect } from '@playwright/test';
 test('Verificar título de Playwright', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.getByAltText('Playwright logo').first()).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
+  await expect(page.getByRole('button', { name: /Search/ })).toBeVisible();
+  await page.screenshot({path:"captura.png"})
 });
 
