e832aaa2b5c8a68540a257865d55c417fbb05539
andrea <andrea.isabelgomero@gmail.com>
2025-09-17 17:05:48 -0600

    cambios

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,8 +3,17 @@ import { test, expect } from '@playwright/test';
 test('Verificar título de Playwright', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
+})
+test('Verificar el logo', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
   await expect(page.getByAltText('Playwright logo').first()).toBeVisible();
+  })
+  test('Verificar el boton node.js', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
   await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
+  })
+  test('Verificar el boton de busqueda', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
   await expect(page.getByRole('button', { name: /Search/ })).toBeVisible();
   await page.screenshot({path:"captura.png"})
 });
